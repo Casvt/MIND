2aa1258500507b6a46d08952628d9dca18b0da79
CasVT <casvantijn@gmail.com>
2025-08-25 21:43:01 +0200

    Refactored ui.py

--- frontend/ui.py
+++ frontend/ui.py
@@ -9,53 +9,58 @@ from flask import Blueprint, render_template, send_file
 from backend.internals.server import Server
 
 ui = Blueprint('ui', __name__)
-methods = ['GET']
 
 
 def render(filename: str, **kwargs: Any) -> str:
     return render_template(filename, url_prefix=Server.url_prefix, **kwargs)
 
 
-@ui.route('/manifest.json', methods=methods)
+@ui.route('/manifest.json')
 def ui_manifest():
-    return send_file(
-        BytesIO(dumps(
+    # autopep8: off
+    manifest = {
+        "name": "MIND",
+        "short_name": "MIND",
+        "description":
+            "MIND is a simple self hosted reminder application that can send "
+            "push notifications to your device. Set the reminder and forget "
+            "about it!",
+        "display": "standalone",
+        "orientation": "portrait-primary",
+        "start_url": f"{Server.url_prefix}/",
+        "scope": f"{Server.url_prefix}/",
+        "id": f"{Server.url_prefix}/",
+        "background_color": "#1b1b1b",
+        "theme_color": "#6b6b6b",
+        "icons": [
             {
-                "name": "MIND",
-                "short_name": "MIND",
-                "description": "MIND is a simple self hosted reminder application that can send push notifications to your device. Set the reminder and forget about it!",
-                "display": "standalone",
-                "orientation": "portrait-primary",
-                "start_url": f"{Server.url_prefix}/",
-                "scope": f"{Server.url_prefix}/",
-                "id": f"{Server.url_prefix}/",
-                "background_color": "#1b1b1b",
-                "theme_color": "#6b6b6b",
-                "icons": [
-                    {
-                        "src": f"{Server.url_prefix}/static/img/favicon.svg",
-                        "type": "image/svg+xml",
-                        "sizes": "any"
-                    }
-                ]
-            },
-            indent=4
-        ).encode('utf-8')),
+                "src": f"{Server.url_prefix}/static/img/favicon.svg",
+                "type": "image/svg+xml",
+                "sizes": "any"
+            }
+        ]
+    }
+    # autopep8: on
+
+    return send_file(
+        BytesIO(
+            dumps(manifest, indent=4).encode('utf-8')
+        ),
         mimetype="application/manifest+json",
         download_name="manifest.json"
     ), 200
 
 
-@ui.route('/', methods=methods)
+@ui.route('/')
 def ui_login():
     return render('login.html')
 
 
-@ui.route('/reminders', methods=methods)
+@ui.route('/reminders')
 def ui_reminders():
     return render('reminders.html')
 
 
-@ui.route('/admin', methods=methods)
+@ui.route('/admin')
 def ui_admin():
     return render('admin.html')
